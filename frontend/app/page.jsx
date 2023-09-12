093760e1701331ada565b8bd889ad5c9c53d4759
Soham Ratnaparkhi <soham.ratnaparkhi@gmail.com>
2023-09-13 00:59:44 +0530

    Revert "feat: home page setup"

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,58 +1,10 @@
 import Link from 'next/link'
-import { HiOutlineArrowRight } from "react-icons/hi"
-import Navbar from './components/Navbar';
-import react from 'react';
 
 export default function Home() {
   return (
-    <div className="w-full h-full bg-white">
-      <Navbar />
-
-      {/* <Link href="/schedule"> Create meeting </Link> */}
-
-      <div className="flex flex-row w-full " >
-        <div className="relative  w-6/12 flex flex-col">
-          <div className='h-40'>
-          </div>
-          <div className='h-80 items-center px-10'>
-            {/* <div className='relative items-center justify-center text-transparent bg-gradient-to-r from-[sec-blue] via-transparent to-[slate-2] bg-clip-text font-bold text-4xl '> */}
-            <div
-              className='relative items-center justify-center font-bold text-4xl'
-              style={{
-                background: 'linear-gradient(180deg, #2D31FA 0%, #04DFFC 50%)',
-                backgroundClip: 'text',
-                WebkitBackgroundClip: 'text',
-                WebkitTextFillColor: 'transparent',
-              }}
-            >
-              Join the Future of Communication
-            </div>
-            <div className='relative items-center justify-center text-black text-2xl Montserrat '>
-              Effortless Video Conferencing
-            </div>
-
-            <button className="flex items-center justify-between text-black font-[500] text-sm border-2 border-sec-blue rounded-full m-3 p-4 px-10 space-x-2
-            hover:bg-light-blue hover:text-black hover:border-white">
-              <span>Schedule a meeting</span>
-              <HiOutlineArrowRight />
-            </button>
-
-
-          </div>
-          <div className='relative h-80'>
-            <div className='absolute h-80 w-6/12 bg-sec-blue rounded-tr-[100%] '>
-              <div className=' absolute h-64 mt-16 mr-16  w-10/12 bg-sky-blue rounded-tr-[100%]'>
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <div className='relative  w-6/12 flex flex-col bg-light-green'>
-          <div className='aboslute bg-sec-blue'>
-            
-          </div>
-        </div>
-      </div>
+    <div>
+      <h1>Home</h1>
+      <Link href="/schedule"> Create meeting </Link>
     </div>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,60 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      'black': '#151D1F',
-      'white': '#FFF',
-      'sec-blue': '#2D31FA',
-      'dark-blue': '#051367',
-      'light-blue': '#5D8BF4',
-      'sky-blue': '#EDF2FE',
-      'light-green': '#95F7B2',
-      'slate-1': '#03C5C7',
-      'slate-2': '#04DFFC',
-    },
-    
     extend: {
-      fontFamily: {
-        'mont': ['var(--font-mont)'],
-      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      height: {
-        '5%': '5%',
-        '10%': '10%',
-        '15%': '15%',
-        '20%': '20%',
-        '25%': '25%',
-        '30%': '30%',
-
-        '75%':'75%',
-        '80%':'80%',
-        '85%':'85%',
-        '90%':'90%',
-        '95%':'95%',
-      },
-      width: {
-        '5%': '5%',
-        '10%': '10%',
-        '15%': '15%',
-        '20%': '20%',
-        '25%': '25%',
-        '30%': '30%',
-
-        '75%':'75%',
-        '80%':'80%',
-        '85%':'85%',
-        '90%':'90%',
-        '95%':'95%',
       },
     },
   },
